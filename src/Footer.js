8906bd32d3abdbe3ae2e85a9d14426b7b81928ef
rudra-t <rtiwari1308@gmail.com>
2023-06-24 12:36:58 +0530

    inial commit

--- src/Footer.js
+++ src/Footer.js
@@ -6,23 +6,17 @@ function Footer() {
         <div>
           <div className="heading2">
             <div className="social-link">
-              <a
-                className=""
-                href="https://www.facebook.com/profile.php?id=100031116935428"
-              >
+              <a className="" href="#">
                 <i class="fa-brands fa-facebook-f"></i>
               </a>
-              <a
-                className=""
-                href="https://www.linkedin.com/in/rudra-tiwari-219bb722b/"
-              >
+              <a className="" href="#">
                 <i class="fa-brands fa-instagram"></i>
               </a>
-              <a className="" href="https://www.instagram.com/rptiwari0/">
+              <a className="" href="#">
                 <i class="fa-brands fa-linkedin"></i>
               </a>
               <a href="#">
-                <i className="fa-brands fa-github"></i>
+                <i className="fa-brands fa-twitter"></i>
               </a>
             </div>
             <p>Design and Developed by Rudra Tiwari</p>
